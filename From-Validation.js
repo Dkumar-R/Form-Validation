4b9f2d9304097771f42e93faa3e6e1a1807e00a6
dk1402 <102382512+Dkumar-R@users.noreply.github.com>
2022-07-23 04:12:54 +0530

    add event listener

---
+++ From-Validation.js
@@ -0,0 +1,5 @@
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmpasswordEl = document.querySelector('#confirm-password');
+const form = document.querySelector('#signup');
